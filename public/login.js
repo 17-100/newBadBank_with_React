583b36458778dd3db3c8362f744dc885a538b23b
17-100 <sebisteri@outlook.com>
2021-10-20 00:10:42 +0200

    adds login form with CreateAccount-functionality

--- public/login.js
+++ public/login.js
@@ -1,11 +1,80 @@
 function Login() {
+    const [show, setShow] = React.useState(true);
+    const [status, setStatus] = React.useState(''); 
+
     return (
         <Card
             bgcolor="primary"
-            header="Login"
-            txtcolor=""
-            body=""
-            text="Log dich olum"
+            header="Create Account"
+            status={status}
+            body={show ? 
+                    <CreateForm setShow={setShow}/> 
+                    : 
+                    <CreateMsg setShow={setShow}/>    
+                }
+            text="create your account olum"
         />
     )
+}
+
+function CreateMsg(props) {
+    return (<>
+        <h5>Success</h5>
+        <button type="submit" 
+                className="btn btn-light" 
+                onClick={() => props.setShow(true)}
+        >Add another Account
+        </button>
+    </>)
+}
+
+function CreateForm(props) {
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    function handle() {
+        console.log(name, email, password);
+        const url = `/account/create/${name}/${email}/${password}`;
+        (async() => {
+            var res = await fetch(url);
+            var data = res.json();
+            console.log(data);
+        })();
+        props.setShow(false);
+    }
+
+    return (<>
+        Name<br/>
+        <input  type="input"
+                className="form-control"
+                placeholder="Enter name"
+                value={name}
+                onChange={e => setName(e.currentTarget.value)}
+        />
+        <br/>
+
+        Email address<br/>
+        <input  type="input"
+                className="form-control"
+                placeholder="Enter Email"
+                value={email}
+                onChange={e => setEmail(e.currentTarget.value)}
+        />
+        <br/>
+
+        Password<br/>
+        <input  type="password"
+                className="form-control"
+                placeholder="Enter password"
+                value={password}
+                onChange={e => setPassword(e.currentTarget.value)}
+        />
+        <br/>
+        <button type="submit"
+                className="btn btn-light"
+                onClick={handle}
+        >Create Account
+        </button>
+    </>)
 }
